d383240c4cc1ab7da06c763b2a538796abb9ad2a
nextdev2 <nextdev0.02@gmail.com>
2023-09-20 19:59:58 +0600

    desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,43 @@
 // FAQ
 
+const questionAnswers = document.querySelectorAll('.question-answer');
 
-const questions = document.querySelectorAll('.question-answer');
+questionAnswers.forEach(function(questionAnswer) {
+    const questionTitle = questionAnswer.querySelector('.title-question');
+    const btn = questionAnswer.querySelector('.question-btn');
+    const answer = questionAnswer.querySelector('.answer');
 
-questions.forEach(function(question) {
-    const btn = question.querySelector('.question-btn');
-    btn.addEventListener("click", function() {
-        questions.forEach(function(item) {
-            if (item !== question) {
+    questionTitle.addEventListener("click", function() {
+        questionAnswers.forEach(function(item) {
+            if (item !== questionAnswer) {
                 item.classList.remove("show-text");
             }
         })
-        question.classList.toggle("show-text");
-    })
-})
+        questionAnswer.classList.toggle("show-text");
+        toggleAnswerVisibility(questionAnswer);
+    });
+
+    btn.addEventListener("click", function(e) {
+        e.stopPropagation();
+        questionAnswers.forEach(function(item) {
+            if (item !== questionAnswer) {
+                item.classList.remove("show-text");
+            }
+        })
+        questionAnswer.classList.toggle("show-text");
+        toggleAnswerVisibility(questionAnswer);
+    });
+
+    // Function to smoothly toggle answer visibility
+    function toggleAnswerVisibility(element) {
+        const answer = element.querySelector('.answer');
+        if (element.classList.contains('show-text')) {
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+        } else {
+            answer.style.maxHeight = '0';
+        }
+    }
+
+    // Initialize answer visibility on page load
+    toggleAnswerVisibility(questionAnswer);
+});
